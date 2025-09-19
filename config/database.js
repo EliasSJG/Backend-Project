afb27f8a551080e48cef2e5546ac5e4459bf4d44
Elias <you@example.com>
2025-09-19 11:56:31 +0200

    file commits with base code

---
+++ config/database.js
@@ -0,0 +1,16 @@
+import mysql from "mysql2/promise";
+
+let connection;
+
+export const getConnection = async () => {
+  if (!connection) {
+    connection = await mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "abc123",
+      database: "animals_db",
+    });
+  }
+  return connection;
+};
